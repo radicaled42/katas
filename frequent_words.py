e77115144a6caffbe437e8898700997ad8aa09aa
radicaled <daniel.bianco@outlook.com>
2024-10-16 19:09:18 -0300

    feat: working solution

--- frequent_words.py
+++ frequent_words.py
@@ -1,22 +1,22 @@
 import re
 
 def top_3_words(text):
-    pattern = r"[#\\/\.,]"
-    
+    pattern = r"[#\\/\.,_!:\-?;]"
+    clean_text = re.sub(pattern, ' ', text).lower()
+        
     repeated_dict = {}
-    splitted_text = text.split()
+    splitted_text = clean_text.split()
     
-    for index in range(0, len(splitted_text)):
-        word = re.sub(pattern, '', splitted_text[index]).lower()
-
+    for word in splitted_text:
         if word not in '' and re.search('[a-zA-Z]', word):
             if word in repeated_dict:
                 repeated_dict[word] += 1
             else:
                 repeated_dict[word] = 1
 
-    sorted_dict = dict(sorted(repeated_dict.items(), key=lambda item: item[1], reverse=True))
-    sorted_list = list(sorted_dict.keys())[:3]
+    #sorted_dict = dict(sorted(repeated_dict.items(), key=lambda item: item[1], reverse=True))
+    #sorted_list = list(sorted_dict.keys())[:3]
+    sorted_list = list(dict(sorted(repeated_dict.items(), key=lambda item: item[1], reverse=True)).keys())[:3]
     print(sorted_list)
     return sorted_list
     
@@ -36,11 +36,20 @@ def top_3_words(text):
 #        coursing. An olla of rather more beef than mutton, a salad on most
 #        nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
 #        on Sundays, made away with three-quarters of his income.""")#, ["a", "of", "on"])
-top_3_words("""IBZpck'pUm::-IBZpck'pUm
-            /.IBZpck'pUm /.IBZpck'pUm,_;. IBZpck'pUm?!/r'Mhr? .IBZpck'pUm
-            IBZpck'pUm-;!-,IBZpck'pUm;/.IBZpck'pUm;r'Mhr!-?/IBZpck'pUm!IBZpck'pUm:!r'Mhr_::IBZpck'pUm.;,!
-            IBZpck'pUm-; _ IBZpck'pUm/
-            ;:-IBZpck'pUm-,IBZpck'pUm,.r'Mhr,/,-r'Mhr, : /r'Mhr:
-            ,-_IBZpck'pUm:IBZpck'pUm_!//;IBZpck'pUm!IBZpck'pUm?-.r'Mhr;,IBZpck'pUm
-            //?!r'Mhr_!r'Mhr:_;?!IBZpck'pUm:-_/r'Mhr?:r'Mhr!._IBZpck'pUm-
-            :r'Mhr_-  IBZpck'pUm,-r'Mhr?;.;""") #['gcxfguepa', 'tlfbvwd', 'xxa']
+#top_3_words("""IEZUgcXu?  _tLFBVwd/,tLFBVwd  XxA/!
+#            _FAUOSO!:cHw?gcxFGUepa :/XxA_;XiJ -!
+#            cHw?tLFBVwd_.;__XxA!:_tLFBVwd-!XiJ/!:..XxA-/ ,:FAUOSO:FAUOSO-!?
+#            ?IEZUgcXu _,_tLFBVwd_!,;gcxFGUepa!cHw?tLFBVwd -gcxFGUepa-: XxA  ?;gcxFGUepa/tLFBVwd,!XxA, /
+#            gcxFGUepa_:.gcxFGUepa!--IEZUgcXu.;:?.VygjyM!-FAUOSO-:FAUOSO:_!_gcxFGUepa?:?-gcxFGUepa!,
+#            ;tLFBVwd. ,tLFBVwd_/, ,tLFBVwd,://
+#            cHw?gcxFGUepa_-gcxFGUepa:FAUOSO!:!!cHw- gcxFGUepa! :? XiJ!,
+#            !_cHw;.::!cHw_:/;gcxFGUepa- /?.cHw_,-;!XxA.?!tLFBVwd-,;--tLFBVwd
+#            -?gcxFGUepa!:??!XiJ/XxA?/-XiJ:.,/-tLFBVwd/gcxFGUepa-/!gcxFGUepa?
+#            tLFBVwd;:_:;XxA-,,-XiJ- ;tLFBVwd/IEZUgcXu/_gcxFGUepa,;tLFBVwd;tLFBVwd?!FAUOSO/.?!gcxFGUepa./gcxFGUepa?,?
+#            gcxFGUepa!cHw!?gcxFGUepa/gcxFGUepa_;tLFBVwd;-;-tLFBVwd!?:IEZUgcXu::?
+#            !XxA  gcxFGUepa  .XiJ__ :tLFBVwd;-,;FAUOSO_.
+#            cHw-_!!FAUOSO/:-gcxFGUepa:.-.cHw.;,!-gcxFGUepa
+#            !?- tLFBVwd-:::,cHw XiJ!.,/cHw.,/gcxFGUepa.;-!gcxFGUepa;?..FAUOSO/?_?-XxA/
+#            .:?gcxFGUepa-?/tLFBVwd: !.:tLFBVwd/_tLFBVwd;/tLFBVwd/_,tLFBVwd;;-/tLFBVwd!.!cHw/
+#            _XxA?;.XiJ/--? FAUOSO/;!:gcxFGUepa_,...gcxFGUepa?-:_?tLFBVwd..XxA?-XxA!.;/.XiJ,FAUOSO
+#            :  !""") #['gcxfguepa', 'tlfbvwd', 'xxa'] ['iezugcxu?', '_tlfbvwdtlfbvwd', 'xxa!']
