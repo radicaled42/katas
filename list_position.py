1cb2551c15ea765d688e0c65778d0ae7be87cd9a
radicaled <daniel.bianco@outlook.com>
2024-10-23 17:54:12 -0300

    feat: working solution

--- list_position.py
+++ list_position.py
@@ -1,25 +1,28 @@
-def get_permutations(word_list):
-    if len(word_list) == 0:
-        return []
- 
-    if len(word_list) == 1:
-        return [word_list]
+import itertools
 
-    perm_list = []
- 
-    for index in range(len(word_list)):
-       main_letter = word_list[index]
-       remain_list = word_list[:index] + word_list[index+1:]
-
-       for post_word in get_permutations(remain_list):
-           #print (post_word)
-           perm_list.append([main_letter] + list(post_word))       
-        
-    return perm_list
+#def get_permutations(word_list):
+#    if len(word_list) == 0:
+#        return []
+# 
+#    if len(word_list) == 1:
+#        return [word_list]
+#
+#    perm_list = []
+# 
+#    for index in range(len(word_list)):
+#       main_letter = word_list[index]
+#       remain_list = word_list[:index] + word_list[index+1:]
+#
+#       for post_word in get_permutations(remain_list):
+#           #print (post_word)
+#           perm_list.append([main_letter] + list(post_word))
+#        
+#    return perm_list
 
 def list_position(word):
-    word_list = list(set([''.join(word) for word in get_permutations(word)]))
-    word_list.sort()
+    #word_list = list(set([''.join(word) for word in get_permutations(word)]))
+    word_list = sorted(set([''.join(word) for word in itertools.permutations(word)]))
+    #word_list.sort()
     #print(word_list)
     word_index = word_list.index(word)
     print(word_index+1)
